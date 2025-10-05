a0a21807d0e9ca6d49c5e99a9f8720b3a5af406b
Juanito Pla <jpla@itba.edu.ar>
2025-10-05 14:50:47 -0300

    Agregue un par de pdfs

--- source/drivers/i2c.h
+++ source/drivers/i2c.h
@@ -26,8 +26,16 @@
 //                  genera un stop en el bus)
 // TX -    bit 4: 1 si vas a enviar datos, 0 si vas a leer datos
 // TXAK -  bit 3: 0 para mandar ACK, 1 para mandar NACK
-// RSTA -  bit 2: 1 para generar un repeated start
+// RSTA -  bit 2: (W)1 para generar un repeated start
 // WUEN -  bit 1: 1 habilita el wakeup cuando coincide el address del slave, 0 normal
 // DMAEN-  bit 0: 0 disable, 1 para usar DMA (la memoria)
+
+// I2Cx_S - I2C status: son flags de los estados del modulo. Es de 8bits
+// TCF -   bit 7: (R)se pone en 1 cuando se termina la transmision (1 byte)
+// IAAS -  bit 6: 1 si el modulo fue direccionado como slave
+// BUSY -  bit 5: (R)1 si el bus esta ocupado, 0 bus is idle
+// ARBL -  bit 4: (R)1 si se perdio la condicion de master por colision
+// RAM -   bit 3: NO ENTENDI-NO LO USAMOS CREO
+// SRW -   bit 2: 0 si el master quiere leer del slave o 1 si quiere escribir al slave
 
 #endif /* DRIVERS_I2C_H_ */
